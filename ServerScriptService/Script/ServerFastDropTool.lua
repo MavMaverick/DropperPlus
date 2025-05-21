492cdf60dc601c5af7bb4ad4b100266f62db92b6
MavMaverick <97918848+MavMaverick@users.noreply.github.com>
2025-05-20 21:06:05 -0500

    1. Added the tool drop location feature, two variables within ServerFastDropTool determine distance and height from the player the tool will be dropped 2. Added 'Configuration' object with int object children 'DropLocationOffsetZ' and 'DropLocationOffsetY', allowing the drop location offset values to be edited without changing the code
    Note: The drop location code uses GetPivot() to get the tool's C-frame, and then PivotTo() to move the tool based on its CFrame orientation at the last moment before being dropped.

--- ServerScriptService/Script/ServerFastDropTool.lua
+++ ServerScriptService/Script/ServerFastDropTool.lua
@@ -4,25 +4,32 @@ local DropToolRequest = ReplicatedStorage:WaitForChild("DropToolRequest")
 DropToolRequest.OnServerEvent:Connect(function(player, tool, rightGrip)
 	if not tool or not player then return end
 
-
-	rightGrip:Destroy()
-	tool.Parent = workspace
+	local currentPivot = tool:GetPivot() -- We want then direction of the tool to use later
+	rightGrip:Destroy() -- Destroys weldm delivering the character of attachment of tool
+	tool.Parent = workspace -- Tool no longer a child of character
 
 	local handle = tool.Handle
 	
 	if handle then
+		-- Roblox disables CanCollide while using tool, we want it on so no fall through floor
 		handle.CanCollide = true
 		handle.Anchored = false
-		--handle.CFrame = handle.CFrame * CFrame.new(0, -5, 0)
+		local dropLocationOffsetZ = tool.FastToolDrop.Configuration.DropLocationOffsetZ.Value
+		local dropLocationOffsetY = tool.FastToolDrop.Configuration.DropLocationOffsetY.Value
 
-		-- Step 1: Give player temp network ownership
+		
+		local forwardCFrame = currentPivot * CFrame.new(0, dropLocationOffsetY, dropLocationOffsetZ) --(-2 Z Axis) move 2 studs forward relative to tool's facing (any less not recommended)
+		tool:PivotTo(forwardCFrame)
+
+		-- Step 1: Give player temp network ownership, this allows smooth dropping for player
 		handle:SetNetworkOwner(player)
 
-		-- Step 2: After short delay, return control to server only if it's still in workspace
+		-- Step 2: After short delay, return control to server only if it's still in workspacen (prevents slow movement)
 		task.delay(1, function()
 			if handle:IsDescendantOf(workspace) and tool.Parent == workspace then
 				handle:SetNetworkOwner(nil)
 			end
 		end)
+		
 	end
 end)
